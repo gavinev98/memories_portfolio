2a61eb8c8c61faab3055c7496cc463ac2703214e
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-14 11:41:32 +0000

    setting up connection to our mongo database.

--- server/index.js
+++ server/index.js
@@ -11,5 +11,11 @@ const app = express();
 //setting up bodyParser so that we can properly send a request.
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
-
+//setting up cors.
 app.use(cors());
+
+//setting up mongoDB with cloud atlas version.
+const CONNECTION_URL = 'mongodb+srv://gavinev98:fire1234@cluster0.bxox6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true});
