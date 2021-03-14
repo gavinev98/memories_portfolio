91701ac605e2c7d78591ec82cb29d3f03803df98
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-14 11:26:24 +0000

    setting up bodyParser and cors with express.

--- server/index.js
+++ server/index.js
@@ -4,3 +4,12 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 
+//set up the app and we can now use app to access different methods,
+const app = express();
+
+
+//setting up bodyParser so that we can properly send a request.
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+
+app.use(cors());
