8f88c2dcdde969121e1b3926af2945d3178e3791
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-20 10:58:41 +0000

    getting messages from mongo.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,22 @@
 //all logic will be stored in the controllers.
+//we can now import our model from the model folder for PostMessage
+import PostMessage from '../models/postsMessage.js'
 
-export const getPost = (req, res) => {
+export const getPost = async (req, res) => {
 
-    res.send('posts home');
+    try {
+        const postMessages = await PostMessage.find();
+
+        console.log(postMessages);
+
+        res.status(200).json(postMessages);
+
+    } catch(errror) {
+        res.status(404).json({ message : error.message });
+
+    }
+    
+
 };
 
 
