bb147e0f9eb84618820e0739b4760ac1e2c43ca6
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-21 10:56:39 +0000

    adding css to the app file.

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,24 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import memories from '../src/images/memories_image.png';
 
+import useStyles from './styles';
+
+//importing components
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 
 const App = () => {
+
+    //calling styles class.
+    const classes = useStyles();
+
     return (
 
         <Container maxwidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center" > Memories </Typography>
-                    <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center" > Memories </Typography>
+                    <img className={classes.image} src={memories} alt="memories" height="60" />
 
             </AppBar>
             <Grow in>
