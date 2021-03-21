551d4a5657f12e6d5b031e1d6959594cc676fac6
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-21 11:14:05 +0000

    setting up redux enviroment.

---
+++ client/src/api/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+//setting up a url which is the endpoint to all our posts.
+const url = 'https://localhost:5000/posts';
+
+//creating function to fetch posts via axios.
+export const fetchPosts = () => axios.get(url);
