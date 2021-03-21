8fdae837b52642eb70ac6e9cc82176b999a1b7fc
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-21 11:25:29 +0000

    exporting the posts reducer.

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+
 import App from './App';
 
 
+//firstly we need to create a redux store
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
