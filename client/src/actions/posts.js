b7a5393d52cdaad84a3462df752b2f367b75dc67
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-23 15:40:43 +0000

    setting up asynchronous getPosts function.

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,8 +1,13 @@
 import * as api from '../api';
 
 // Action Creators
-const getPosts = () => {
+//this will be an asynchronous so we will need two AC.
+
+//redux thunk allows us to specify an additional arrow function
+//this will be an asynchronous action creator as we need to fetch the data.
+const getPosts = () => async (dispatch) => {
     const action = { type: 'FETCH_ALL' , payload: []}
 
-    return action;
+    dispatch(action);
 }
+
