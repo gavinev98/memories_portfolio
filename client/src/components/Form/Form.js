68a681632240544dc46e8758e0742c54e4ab1b9f
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-27 14:37:11 +0000

    Using filebase to set selectedFile state.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
-
+import FileBase from 'react-file-base64';
 import useStyles from './styles';
 
 const Form = () => {
@@ -61,6 +61,15 @@ const Form = () => {
             fullWidth 
             value={postData.tags}
             onChange={(e) => setPostData({ ...postData, tags: e.target.value})}/>
+
+            <div className={classes.fileInput}>
+                <FileBase
+                    type="file"
+                    multiple={false}
+                    onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})}
+                    />
+
+            </div>
 
 
             </form>
