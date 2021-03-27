b836387dd7fb128e4b28c8c5e381aade59c220c8
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-27 14:48:01 +0000

    setting up api request to create a new post.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,5 @@ const url = 'http://localhost:5000/posts';
 
 //creating function to fetch posts via axios.
 export const fetchPosts = () => axios.get(url);
+//creating api request to create a new post.
+export const createPost = (newPost) => axios.post(url, newPost);
