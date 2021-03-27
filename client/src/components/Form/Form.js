fa306b70c5b83e423b1c8b741545cfe9869ac914
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-27 14:25:59 +0000

    setting up fields for the memory form.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -28,7 +28,7 @@ const Form = () => {
 
     return (
         <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={classes.from} onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
             <Typography variant="h6">Creating a Memory</Typography>
             <TextField 
             name="creator" 
@@ -37,6 +37,32 @@ const Form = () => {
             fullWidth 
             value={postData.creator}
             onChange={(e) => setPostData({ ...postData, creator: e.target.value})}/>
+
+            <TextField 
+            name="title" 
+            variant="outlined" 
+            label="Title" 
+            fullWidth 
+            value={postData.title}
+            onChange={(e) => setPostData({ ...postData, title: e.target.value})}/>
+
+            <TextField 
+            name="message" 
+            variant="outlined" 
+            label="Message" 
+            fullWidth 
+            value={postData.message}
+            onChange={(e) => setPostData({ ...postData, message: e.target.value})}/>
+            
+            <TextField 
+            name="tags" 
+            variant="outlined" 
+            label="Tags" 
+            fullWidth 
+            value={postData.tags}
+            onChange={(e) => setPostData({ ...postData, tags: e.target.value})}/>
+
+
             </form>
         </Paper>
     );
