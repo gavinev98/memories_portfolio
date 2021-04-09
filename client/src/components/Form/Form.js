4257c323e29ae45031793d8f9bb809822572dcdb
Gavin Everett <gavin.everett48@hotmail.com>
2021-04-09 15:43:00 +0100

    setup useEffect for passing data to form.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import { useDispatch, useSelector } from 'react-redux';
@@ -21,7 +21,10 @@ const Form = ({currentID, setCurrentID}) => {
       //using selector functionality to retrieve the currentID state which is set onclick of the post.
       const post = useSelector((state) => currentID ? state.posts.find((p) => p._id == currentID) : null);
 
-
+    //this useEffect will run when the post varaible changes.
+      useEffect(() => {
+        if(post) setPostData(post);
+      }, [post]);
 
      //creating a state for the post
      const [postData, setPostData] = 
