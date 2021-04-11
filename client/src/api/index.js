b30e78df459e2d23047156de1a794b33778d0d7e
Gavin Everett <gavin.everett48@hotmail.com>
2021-04-11 16:34:54 +0100

    setup api request to delete a post.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,3 +9,5 @@ export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 //creating api request to update a post.
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+//creating api request to delete a post
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
