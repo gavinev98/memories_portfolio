7b175462b920619c01a0c1a15f888e9743dc3de5
Gavin Everett <gavin.everett48@hotmail.com>
2021-04-14 16:34:27 +0100

    creating api call for liking a post.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -11,3 +11,5 @@ export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 //creating api request to delete a post
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
+//creating api request to like a post
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
